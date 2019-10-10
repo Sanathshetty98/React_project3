c223d5eac00fe970e16eef37d71e06f294cc6066
Sanathshetty98 <shetty.sanath98@gmail.com>
2019-10-10 15:58:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     : <User />}
     </React.Fragment>
       /*
+      <BrowserRouter basename="/my-app">
       <BrowserRouter>
         <React.Fragment>
           <nav>
